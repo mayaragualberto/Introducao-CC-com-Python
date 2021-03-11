dc63809ab77696f830c48ffd6123015efb19aee2
Mayara Gualberto <64241581+mayaragualberto@users.noreply.github.com>
2021-03-10 22:16:16 -0300

    Add files via upload

---
+++ Parte1/Semana1/ConversorDeTemperatura.py
@@ -0,0 +1,8 @@
+#O programa deve receber uma temperatura em Fahrenheit e converter em Celsius
+
+temperaturaFahrenheit = input ("Digite uma temperatura em Fahrenheit: ")
+
+
+temperaturaCelsius = (float(temperaturaFahrenheit)-32)*5/9
+
+print ("A temperatura em Celsius é:", temperaturaCelsius)
---
+++ Parte1/Semana1/Educado.py
@@ -0,0 +1,7 @@
+#O programa recebe os nome sãdo pai e me e dá bom dia.
+
+nomeDaMae = input("Qual o nome da sua mãe? ")
+nomeDoPai = input("Qual o nome do seu pai? ")
+
+print ("Bom dia, Sra.", nomeDaMae,"! E bom dia, Sr.", nomeDoPai, ".")
+
